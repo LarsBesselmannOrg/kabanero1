cdf7d4a3bd00c2833fc49d6182de48cf2f0fbc98
Lars Besselmann <32673634+LarsBesselmannIBM@users.noreply.github.com>
2020-07-10 18:51:51 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const app = require('express')()
 
 app.get('/', (req, res) => {
-  res.send("Hello world from my Appsody! - 2020/06/08");
+  res.send("Hello world from my Appsody! - 2020/07/08");
 });
 
 module.exports.app = app;
