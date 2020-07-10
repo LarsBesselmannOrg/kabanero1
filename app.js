3ac6aa4d870bbf13eba17d7ccb5da4d808f5d731
Lars Besselmann <32673634+LarsBesselmannIBM@users.noreply.github.com>
2020-07-10 19:37:32 +0200

    Update for TEC

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const app = require('express')()
 
 app.get('/', (req, res) => {
-  res.send("Hello TEC world from my Appsody! - 2020/07/10");
+  res.send("Hello TEC world from my Appsody!!! - 2020/07/10");
 });
 
 module.exports.app = app;
